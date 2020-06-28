454632a5f151e9c6a7a0511266a572e42ac2cdd3
Zhu Wanjun <zhuwanjun2008@gmail.com>
2020-06-28 14:51:28 +0800

    第一周作业完成

--- week01/spider/spider/spiders/movies.py
+++ week01/spider/spider/spiders/movies.py
@@ -10,25 +10,52 @@ class MoviesSpider(scrapy.Spider):
 
     def start_requests(self):
 
-        for i in range(0, 2):
+        for i in range(0, 10):
             url = f'https://maoyan.com/films?showType=3&offset={i*30}'
             yield scrapy.Request(url=url,callback=self.parse)
 
     def parse(self, response):
 
+        items = []
+
         movies = Selector(response=response).xpath('//div[@class="movie-hover-info"]')
 
         for movie in movies:
+
+            item = SpiderItem()
             
             movie_title = movie.xpath('./div/span/text()').extract_first()
-            print(movie_title)
+            item['movie_title'] = movie_title
 
-            movie_time = movie.xpath('./div[@class="movie-hover-title movie-hover-brief"]/text()').extract()
+            movie_info_list = movie.xpath('./div[@class="movie-hover-title"]/text()').extract()
+            movie_info_list_new = []
 
-            #movie_info = movie.xpath('./div/text()').extract()
-            #print(movie_info)
-            #movie_type = movie_info[4].strip()
-            #movie_time = movie_info[8].strip()
+            for x in movie_info_list:
+                x = x.replace('\n','').replace(' ','')
+                if x != '':
+                    movie_info_list_new.append(x)
 
-            #print(movie_type)
-            print(movie_time)
+            movie_type = movie_info_list_new[0]
+            item['movie_type'] = movie_type
+
+            movie_time_list = movie.xpath('./div[@class="movie-hover-title movie-hover-brief"]/text()').extract()
+            movie_time_list_new = []
+
+            for y in movie_time_list:
+                y = y.replace('\n','').replace(' ','')
+                if y != '':
+                    movie_time_list_new.append(y)
+
+            movie_time = ''.join(movie_time_list_new)
+            if movie_time == '':
+                movie_time = '暂无'
+
+            item['movie_time'] = movie_time
+
+            items.append(item)
+
+        return items
+
+        
+
+        
