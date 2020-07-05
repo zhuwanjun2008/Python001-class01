f7cbb1a9d9f335984933608f2f51ea9565892973
Zhu Wanjun <zhuwanjun2008@gmail.com>
2020-07-05 15:34:04 +0800

    将数据保存到mysql中

--- week02/task1/maoyan/maoyan/pipelines.py
+++ week02/task1/maoyan/maoyan/pipelines.py
@@ -7,6 +7,7 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import pandas as pd
+import pymysql
 
 class MaoyanPipeline:
     def process_item(self, item, spider):
@@ -15,9 +16,30 @@ class MaoyanPipeline:
         movie_type =  item['movie_type']
         movie_time = item['movie_time']
 
-        output = [f'名称:{movie_title},类型: {movie_type},上映时间: {movie_time}']
+        #output = [f'名称:{movie_title},类型: {movie_type},上映时间: {movie_time}']
+        #movie_pd = pd.DataFrame(data = output)
+        #movie_pd.to_csv('./movie.csv', mode='a', encoding='utf8', index=False, header=False)
 
-        movie_pd = pd.DataFrame(data = output)
-        movie_pd.to_csv('./movie.csv', mode='a', encoding='utf8', index=False, header=False)
+        #数据保存方式，调整为mysql
+        conn = pymysql.connect(
+            host = 'localhost', 
+            port = 3306, 
+            user = 'maoyan', 
+            password = '1qaz@WSX', 
+            db = 'maoyan')
+
+        cur = conn.cursor()
+
+        #符合SQL语法，语句中增加双引号
+        sql = f'INSERT INTO movie VALUES ("{movie_title}","{movie_type}","{movie_time}")'
+
+        try:
+            cur.execute(sql)
+            conn.commit()
+        except (Exception) as e:
+            print(e)
+            conn.rollback()
+        
+        conn.close()
 
         return item
---
+++ week02/task1/maoyan/maoyan/sql/04_insert.sql
@@ -0,0 +1,2 @@
+# 插入数据
+INSERT INTO movie VALUES ('movie_title','movie_type','movie_time')
